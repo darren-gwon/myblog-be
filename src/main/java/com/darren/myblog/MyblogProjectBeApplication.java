531f27e3e8c615e68ac493dbbcd9edccbe1b2c22
Darren Gwon <darren.gwon@gmail.com>
2022-06-16 00:30:06 +0900

    [Create]myblog springboot project

---
+++ src/main/java/com/darren/myblog/MyblogProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.darren.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogProjectBeApplication.class, args);
+	}
+
+}
